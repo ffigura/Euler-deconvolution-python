a098552ad68596598beb736ad713d17f8acfc840
ffigura <figura146@gmail.com>
2019-09-30 17:30:57 -0300

    Add files via upload

--- code/test_4_sources/synthetic_test.py
+++ code/test_4_sources/synthetic_test.py
@@ -38,9 +38,8 @@ window size and the percentage of solutions to keep
 winsize=9
 #percentage of the solutions that will be keep
 filt=0.08
-#empty arrays for multiple SIs
+#empty array for multiple SIs
 est_classic=[]
-est_plateau=[]
 #Define below the SIs to be tested
 SI_vet=[0.001,1,2,3]
 '''
