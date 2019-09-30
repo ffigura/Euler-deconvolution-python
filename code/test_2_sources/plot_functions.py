79833db189cbdb613fabc94490307e20ef7a91fd
ffigura <figura146@gmail.com>
2019-09-30 13:59:00 -0300

    Add files via upload
    the plot of Niccoli is no longer used

--- code/test_2_sources/plot_functions.py
+++ code/test_2_sources/plot_functions.py
@@ -17,39 +17,10 @@ email: felipe146@hotmail.com, valcris@on.br
 """
 
 import numpy as np
-import matplotlib as mpl
 import matplotlib.pylab as plt
 import gc
 import matplotlib.patches as patches
 
-'''
-For a nice plot of magnetic anomaly and b-estimates
- following Niccoli (2014)
-'''
-LinL = np.loadtxt('input/Linear_L_0-1.txt')
-
-b3=LinL[:,2] # value of blue at sample n
-b2=LinL[:,2] # value of blue at sample n
-b1=np.linspace(0,1,len(b2)) # position of sample n - ranges from 0 to 1
-
-# setting up columns for list
-g3=LinL[:,1]
-g2=LinL[:,1]
-g1=np.linspace(0,1,len(g2))
-r3=LinL[:,0]
-r2=LinL[:,0]
-r1=np.linspace(0,1,len(r2))
-# creating list
-R=zip(r1,r2,r3)
-G=zip(g1,g2,g3)
-B=zip(b1,b2,b3)
-# transposing list
-RGB=zip(R,G,B)
-rgb=zip(*RGB)
-# creating dictionary
-k=['red', 'green', 'blue']
-LinearL=dict(zip(k,rgb)) # makes a dictionary from 2 lists
-my_cmap = mpl.colors.LinearSegmentedColormap('my_colormap',LinearL) 
 #######################################################################
 
 
@@ -67,14 +38,15 @@ def plot_classic(data,est_classic,xi,yi,zi,shape):
     plt.title('(a)', fontsize = 14, loc='center',y=-0.39)
     
     rect2 = patches.Rectangle((4.050,5.850),0.4,0.4,linewidth=1,
-                              edgecolor='crimson',facecolor='none',
+                              edgecolor='black',facecolor='none',
                               linestyle='-',zorder=2)
     rect3 = patches.Rectangle((5.850,6.850),0.3,0.3,linewidth=1,
-                              edgecolor='crimson',facecolor='none',
+                              edgecolor='black',facecolor='none',
                               linestyle='-',zorder=2)
     im=plt.contourf(yi.reshape(shape)/1000.,xi.reshape(shape)/1000.,
-                   data.reshape(shape), 30, cmap=my_cmap)
+                   data.reshape(shape), 30, cmap='rainbow')
     ax = plt.gca()
+    
     ax.set_ylabel('Northing (km)', fontsize = 14)
     ax.set_xlabel('Easting (km)', fontsize = 14)
     ax.tick_params(labelsize=13)
@@ -85,8 +57,8 @@ def plot_classic(data,est_classic,xi,yi,zi,shape):
     cbar.ax.tick_params(labelsize=13)
     ax.set_xlim(np.min(yi/1000.),np.max(yi/1000.))
     ax.set_ylim(np.min(xi/1000.),np.max(xi/1000.))
-    plt.text(8,8,'P2',color='w', size='large')
-    plt.text(2,4,'P3',color='w', size='large')
+    plt.text(8,8,'P2',color='k', size='large')
+    plt.text(2,4,'P3',color='k', size='large')
     
     minz = np.min(0.)
     maxz = np.max(1.2)
@@ -128,7 +100,7 @@ def plot_classic(data,est_classic,xi,yi,zi,shape):
     ax.set_xlabel('Easting (km)', fontsize = 14)
     ax.tick_params(labelsize=13)
     scat=plt.scatter(est_classic[0][:,1]/1000., est_classic[0][:,0]/1000.,
-                     s=50,c=(est_classic[0][:,3]/1000.), cmap=my_cmap,
+                     s=50,c=(est_classic[0][:,3]/1000.), cmap='rainbow',
                      vmin=minb, vmax=maxb,edgecolors='k')            
     cbar=plt.colorbar(scat,ticks=levelsb,pad=0.01,shrink=1,format='%0.2f')
     cbar.set_label('$\^b$ (nT)',labelpad=-18,y=-0.05, rotation=0,
@@ -172,7 +144,7 @@ def plot_classic(data,est_classic,xi,yi,zi,shape):
     ax.set_xlabel('Easting (km)', fontsize = 14)
     ax.tick_params(labelsize=13)
     scat=plt.scatter(est_classic[1][:,1]/1000., est_classic[1][:,0]/1000.,
-                     s=50,c=(est_classic[1][:,3]/1000.), cmap=my_cmap,
+                     s=50,c=(est_classic[1][:,3]/1000.), cmap='rainbow',
                      vmin=minb, vmax=maxb,edgecolors='k')            
     cbar=plt.colorbar(scat,ticks=levelsb,pad=0.01,shrink=1,format='%0.2f')
     cbar.set_label('$\^b$ (nT)',labelpad=-18,y=-0.05, rotation=0,
