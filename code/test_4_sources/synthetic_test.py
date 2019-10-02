561ac5ed7aa72004ddacad765e7986af4c40af65
ffigura <figura146@gmail.com>
2019-10-02 18:32:31 -0300

    Add files via upload

--- code/test_4_sources/synthetic_test.py
+++ code/test_4_sources/synthetic_test.py
@@ -14,6 +14,28 @@ authors: Felipe F. Melo and Valeria C.F. Barbosa, 2019
 email: felipe146@hotmail.com, valcris@on.br
 """
 
+"""
+Parameters:
+
+Size of the moving data window:    
+    winsize - an odd integer number. 
+              Ex.: for a moving data window of 5 x 5 grid points -> winsize = 5
+                                  
+Percentage of the solutions that will be keep:
+    filt - a float number ranging from 0.0 to 1.0. 
+           Ex.: to keep 10% of the solutions -> filt = 0.1
+
+Structural indices used:
+    SI_vet - an array that can store any of the four SIs.
+             Ex.: to test only the SI = 1 -> SI_vet = [1]
+                  to test the four SIs -> SI_vet = [0.01,1,2,3]
+
+The areas to compute the statistics about the mean of the northing,
+easting and depth estimates:
+    area_cla  - array defining the four vertices of a polygon 
+                [south,north,west,east]
+"""
+
 import numpy as np
 import plot_functions as plt_fc
 import euler_python as euler
